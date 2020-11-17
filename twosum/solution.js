ad32fbe53eccc9ce5d788f2b161b28bca21e7d82
cortezd334 <cortezd334@gmail.com>
2020-11-16 21:04:19 -0800

    updated

--- twosum/solution.js
+++ twosum/solution.js
@@ -1,12 +1,16 @@
-var twoSum = function(nums, target) {
+function twoSum(nums, target) {
     const len = nums.length
     let output = []
-    for(let i = 0; i < len - 1; i++){
-        for( let j = 1; j < len - 2; j++){
-            if(nums[i] + nums[j] === target){
+    for(let i = 0; i < len; i++){
+        for( let j = 1; j < len; j++){
+            if(nums[i] + nums[j] === target && i != j){
             output = [i, j]
             }
         }
     }
     return output
 };
+
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
