d75572875914ee733a4059773666063d2e3543bd
cortezd334 <cortezd334@gmail.com>
2020-11-24 18:53:35 -0800

    reverse with modulo

---
+++ reverseinteger/solution.js
@@ -0,0 +1,28 @@
+const reverse = (x) => {
+    console.log(x)
+    const neg = x < 0
+    let num = neg ? (x * -1) : x
+    let newX = neg ? '-' : ''
+    if(num == 0){
+        return 0
+    } else {
+        while(num > 0){
+        let digit = num % 10
+        num = Math.floor(num/10)
+        newX += digit.toString()
+        }
+        let result = +newX
+        if(-(2**31) <= result && result <= ((2**31)-1)){
+            return result
+        } else {
+            return 0
+        }
+    }
+
+}
+
+console.log(reverse(123))
+console.log(reverse(-123))
+console.log(reverse(120))
+console.log(reverse(0))
+console.log(reverse(1534236469))
